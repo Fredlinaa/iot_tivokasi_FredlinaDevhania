8aeab9f4fc78873360ab77b0658232728c9b38f1
Fredlina Devhania Kholishah <devhania88@gmail.com>
2025-06-16 17:09:44 +0700

    first commit

---
+++ LaprakIOT_minggu1_Fredlina_Devhania/src/main.cpp
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("Hello, ESP32!");
+  pinMode(23, OUTPUT);
+  pinMode(22, OUTPUT);
+  pinMode(21, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(23, HIGH);
+  digitalWrite(22, LOW);
+  digitalWrite(21, LOW);
+  delay(30000); 
+  digitalWrite(23, LOW);
+  digitalWrite(22, HIGH);
+  digitalWrite(21, LOW);
+  delay(20000);
+  digitalWrite(23, LOW);
+  digitalWrite(22, LOW);
+  digitalWrite(21, HIGH);
+  delay(5000);
+}
